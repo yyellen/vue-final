d78356a6307de4c03d636d7bde3bf2cfd70bb41f
S2 資訊技術中心 王彥婷 <yyopenfolder@gmail.com>
2022-10-28 17:27:20 +0800

    add nest router

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,13 @@ const routes = [
   {
     path: '/dashboard',
     name: 'dashboard',
-    component: () => import('../views/DashboardView.vue')
+    component: () => import('../views/DashboardView.vue'),
+    children: [
+      {
+        path: 'products',
+        component: () => import('../views/ProductsView.vue')
+      }
+    ]
   }
 ]
 
