27d3970ee8bd95445677726ea52e95bbb38c9909
S2 資訊技術中心 王彥婷 <yyopenfolder@gmail.com>
2022-10-28 17:28:22 +0800

    create `NavBar` & logout

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>Dashboard</div>
+  <div>
+    <Navbar />
+    <router-view />
+  </div>
 </template>
 
 <script>
+import Navbar from '../components/NavBar.vue'
+
 export default {
+  components: {
+    Navbar
+  },
   created () {
     const token = document.cookie.replace(
       /(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/,
