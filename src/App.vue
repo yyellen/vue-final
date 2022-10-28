809276295749104780a9ac6de5d599ac92040d0b
S2 資訊技術中心 王彥婷 <yyopenfolder@gmail.com>
2022-10-28 17:26:55 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
-@import "./assets/all.scss";
+@import './assets/all.scss';
 </style>
