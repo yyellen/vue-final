966eaf97570d584a8a0298c50e9ab0389eb06b15
S2 資訊技術中心 王彥婷 <yyopenfolder@gmail.com>
2022-10-31 17:46:02 +0800

    remove console.log from DashboardView

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <Navbar />
-    <router-view />
+    <div class="container-fluid">
+      <router-view />
+    </div>
   </div>
 </template>
 
@@ -17,12 +19,9 @@ export default {
       /(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/,
       '$1'
     )
-    console.log(token)
     this.axios.defaults.headers.common.Authorization = token
     const api = `${process.env.VUE_APP_API}api/user/check`
-    console.log(api)
     this.axios.post(api, this.user).then(res => {
-      console.log(res)
       if (!res.data.success) {
         this.$router.push('/login')
       }
