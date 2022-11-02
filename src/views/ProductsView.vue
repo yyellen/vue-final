9c77c654ffc0acaa9e4b9058f85370119fbee623
S2 資訊技術中心 王彥婷 <yyopenfolder@gmail.com>
2022-11-02 16:42:19 +0800

    透過彈出視窗新增品項: methods openModal()

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="text-end">
-    <button
-      class="btn btn-primary"
-      type="button"
-      @click="$refs.productModal.showModal"
-    >
+    <button class="btn btn-primary" type="button" @click="openModal">
       增加一個產品
     </button>
   </div>
@@ -38,7 +34,7 @@
       </tr>
     </tbody>
   </table>
-  <ProductModal ref="productModal" />
+  <ProductModal ref="productModal"></ProductModal>
 </template>
 
 <script>
@@ -48,7 +44,8 @@ export default {
   data () {
     return {
       products: [],
-      pagination: {}
+      pagination: {},
+      tempProduct: {}
     }
   },
   components: {
@@ -65,11 +62,18 @@ export default {
         }
       })
     },
+    openModal () {
+      this.tempProduct = {}
+      const productComponent = this.$refs.productModal
+      productComponent.showModal()
+    },
     updateProduct (item) {
       this.tempProduct = item
       const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product`
+      const productComponent = this.$refs.productModal
       this.axios.post(api, { data: this.tempProduct }).then(res => {
         console.log(res)
+        productComponent.hideModal()
         this.getProducts()
       })
     }
