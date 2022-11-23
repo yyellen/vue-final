494542662334adefa3f5cfc310a98f21ec082cc0
yyellen <yyopenfolder@gmail.com>
2022-11-23 11:02:06 +0800

    修改路徑/

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    // component: HomeView
+    component: () => import('../views/LoginView.vue')
   },
   {
     path: '/about',
