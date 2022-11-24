46dfb5061c6368dd85bf8af28b16ab66868553de
yyellen <yyopenfolder@gmail.com>
2022-11-24 11:08:21 +0800

    feat: 利用 app.config.globalProperties 套用全域的千分號

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-// Import component
 import Loading from 'vue3-loading-overlay'
-// Import stylesheet
 import 'vue3-loading-overlay/dist/vue3-loading-overlay.css'
+import { currency } from './methods/filters'
 
 const app = createApp(App)
+app.config.globalProperties.$filters = {
+  currency
+}
 app.use(VueAxios, axios)
 app.use(router)
 app.component('LoadingCircle', Loading)
