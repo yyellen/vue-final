d39872c90b9f582bc037078d6d8bbbcb71516b81
yyellen <yyopenfolder@gmail.com>
2022-11-30 17:55:44 +0800

    pref: customize Bootstrap primary color

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">範例作品</a>
       <button
