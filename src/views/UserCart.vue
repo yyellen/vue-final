afd48c9586990b7fe6c3aead52194523ae8d0821
yyellen <yyopenfolder@gmail.com>
2022-12-07 18:02:09 +0800

    feat: 用戶端建立訂單 模板

--- src/views/UserCart.vue
+++ src/views/UserCart.vue
@@ -164,6 +164,62 @@
           </div>
         </div>
       </div>
+      <!-- 用戶端建立訂單 -->
+      <div class="my-5 row justify-content-center">
+        <form class="col-md-6">
+          <table class="table align-middle">
+            <thead>
+              <th>品名</th>
+              <th>數量</th>
+              <th>單價</th>
+            </thead>
+            <tbody>
+              <tr>
+                <td>產品</td>
+                <td>1 / 個</td>
+                <td class="text-end">100</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colspan="2" class="text-end">總計</td>
+                <td class="text-end">100</td>
+              </tr>
+            </tfoot>
+          </table>
+
+          <table class="table">
+            <tbody>
+              <tr>
+                <th width="100">Email</th>
+                <td>abc@gmail.com</td>
+              </tr>
+              <tr>
+                <th>姓名</th>
+                <td>AA</td>
+              </tr>
+              <tr>
+                <th>收件人電話</th>
+                <td>0987654321</td>
+              </tr>
+              <tr>
+                <th>收件人地址</th>
+                <td>Address</td>
+              </tr>
+              <tr>
+                <th>付款狀態</th>
+                <td>
+                  <span>尚未付款</span>
+                  <span class="text-success">付款完成</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="text-end">
+            <button class="btn btn-danger">確認付款去</button>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
