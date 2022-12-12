95f16e23d8e23938c081add29bf48af11dd465a6
yyellen <yyopenfolder@gmail.com>
2022-12-12 11:02:33 +0800

    feat: 用戶端建立訂單 vee-validate表單驗證

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -8,6 +9,28 @@ import 'vue3-loading-overlay/dist/vue3-loading-overlay.css'
 import { currency, date } from './methods/filters'
 import $httpMessageState from './methods/pushMessageState'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+// 匯入 vee-validate 主套件
+import { Field, Form, ErrorMessage, defineRule, configure } from 'vee-validate'
+// 匯入 vee-validate 相關規則
+import { required, email, min } from '@vee-validate/rules'
+// 匯入多國語系的功能
+import { localize, setLocale } from '@vee-validate/i18n'
+// 匯入繁體中文語系檔案
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json'
+
+// 定義驗證規則
+defineRule('required', required)
+defineRule('email', email)
+defineRule('min', min)
+
+// 設定 vee-validate 全域規則
+configure({
+  generateMessage: localize({ zh_TW: zhTW }), // 載入繁體中文語系
+  validateOnInput: true // 當輸入任何內容直接進行驗證
+})
+
+// 設定預設語系
+setLocale('zh_TW')
 
 const app = createApp(App)
 // 自定義的屬性名稱前方加上$，避免和區域元件內的變數產生衝突
@@ -21,4 +44,7 @@ app.config.globalProperties.$httpMessageState = $httpMessageState
 app.use(VueAxios, axios)
 app.use(router)
 app.component('LoadingCircle', Loading)
+app.component('Form', Form)
+app.component('Field', Field)
+app.component('ErrorMessage', ErrorMessage)
 app.mount('#app')
--- src/views/UserCart.vue
+++ src/views/UserCart.vue
@@ -166,59 +166,82 @@
       </div>
       <!-- 用戶端建立訂單 -->
       <div class="my-5 row justify-content-center">
-        <form class="col-md-6">
-          <table class="table align-middle">
-            <thead>
-              <th>品名</th>
-              <th>數量</th>
-              <th>單價</th>
-            </thead>
-            <tbody>
-              <tr>
-                <td>產品</td>
-                <td>1 / 個</td>
-                <td class="text-end">100</td>
-              </tr>
-            </tbody>
-            <tfoot>
-              <tr>
-                <td colspan="2" class="text-end">總計</td>
-                <td class="text-end">100</td>
-              </tr>
-            </tfoot>
-          </table>
-
-          <table class="table">
-            <tbody>
-              <tr>
-                <th width="100">Email</th>
-                <td>abc@gmail.com</td>
-              </tr>
-              <tr>
-                <th>姓名</th>
-                <td>AA</td>
-              </tr>
-              <tr>
-                <th>收件人電話</th>
-                <td>0987654321</td>
-              </tr>
-              <tr>
-                <th>收件人地址</th>
-                <td>Address</td>
-              </tr>
-              <tr>
-                <th>付款狀態</th>
-                <td>
-                  <span>尚未付款</span>
-                  <span class="text-success">付款完成</span>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <div class="text-end">
-            <button class="btn btn-danger">確認付款去</button>
+        <Form class="col-md-6" v-slot="{ errors }" @submit="createOrder">
+          <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <Field
+              id="email"
+              name="email"
+              type="email"
+              class="form-control"
+              :class="{ 'is-invalid': errors['email'] }"
+              placeholder="請輸入 Email"
+              rules="email|required"
+              v-model="form.user.email"
+            ></Field>
+            <ErrorMessage name="email" class="invalid-feedback"></ErrorMessage>
           </div>
-        </form>
+
+          <div class="mb-3">
+            <label for="name" class="form-label">收件人姓名</label>
+            <Field
+              id="name"
+              name="姓名"
+              type="text"
+              class="form-control"
+              :class="{ 'is-invalid': errors['姓名'] }"
+              placeholder="請輸入姓名"
+              rules="required"
+              v-model="form.user.name"
+            ></Field>
+            <ErrorMessage name="姓名" class="invalid-feedback"></ErrorMessage>
+          </div>
+
+          <div class="mb-3">
+            <label for="tel" class="form-label">收件人電話</label>
+            <Field
+              id="tel"
+              name="電話"
+              type="tel"
+              class="form-control"
+              :class="{ 'is-invalid': errors['電話'] }"
+              placeholder="請輸入電話"
+              :rules="isPhone"
+              v-model="form.user.tel"
+            ></Field>
+            <ErrorMessage name="電話" class="invalid-feedback"></ErrorMessage>
+          </div>
+
+          <div class="mb-3">
+            <label for="address" class="form-label">收件人地址</label>
+            <Field
+              id="address"
+              name="地址"
+              type="text"
+              class="form-control"
+              :class="{ 'is-invalid': errors['地址'] }"
+              placeholder="請輸入地址"
+              rules="required"
+              v-model="form.user.address"
+            ></Field>
+            <ErrorMessage name="地址" class="invalid-feedback"></ErrorMessage>
+          </div>
+
+          <div class="mb-3">
+            <label for="message" class="form-label">留言</label>
+            <textarea
+              name=""
+              id="message"
+              class="form-control"
+              cols="30"
+              rows="10"
+              v-model="form.message"
+            ></textarea>
+          </div>
+          <div class="text-end">
+            <button class="btn btn-danger">送出訂單</button>
+          </div>
+        </Form>
       </div>
     </div>
   </div>
@@ -232,6 +255,15 @@ export default {
       product: {},
       status: {
         loadingItem: '' // 對應品項id
+      },
+      form: {
+        user: {
+          name: '',
+          email: '',
+          tel: '',
+          address: ''
+        },
+        message: ''
       },
       cart: {},
       coupon_code: ''
@@ -308,6 +340,20 @@ export default {
         this.getCart()
         this.isLoading = false
       })
+    },
+    createOrder() {
+      const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/order`
+      const order = this.form
+      this.$http.post(url, { data: order }).then(res => {
+        console.log(res)
+      })
+    },
+    isPhone(value) {
+      if (!value) {
+        return '電話 為必填'
+      }
+      const phoneNumber = /^(09)[0-9]{8}$/
+      return phoneNumber.test(value) ? true : '電話 須為有效的電話號碼'
     }
   },
   created() {
