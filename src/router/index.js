27a16d286a990e0e59f54c0fa39d40e8b75f1ac9
yyellen <yyopenfolder@gmail.com>
2022-12-12 14:32:26 +0800

    feat: 用戶端訂單確認頁面

--- src/router/index.js
+++ src/router/index.js
@@ -53,6 +53,10 @@ const routes = [
       {
         path: 'product/:productId',
         component: () => import('../views/UserProduct.vue')
+      },
+      {
+        path: 'checkout/:orderId',
+        component: () => import('../views/UserCheckout.vue')
       }
     ]
   }
